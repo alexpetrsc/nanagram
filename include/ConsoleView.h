af9e483ed50697ed02ecdfc9abd09307d9f29e70
alexpetrsc <alexpetrsc@proton.me>
2026-07-25 22:42:46 +0300

    Create ConsoleView.h

---
+++ include/ConsoleView.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+
+class ConsoleView{
+
+public:
+
+    void print(
+
+        const std::map<
+
+            std::string,
+
+            std::vector<std::string>
+
+        >&
+
+    );
+
+};
