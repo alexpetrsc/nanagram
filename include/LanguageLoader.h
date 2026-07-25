2c482498741326ebc56160a8887ae182238669c1
alexpetrsc <alexpetrsc@proton.me>
2026-07-25 22:41:28 +0300

    Create LanguageLoader.h

---
+++ include/LanguageLoader.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class LanguageLoader{
+
+public:
+
+    std::vector<std::string>
+
+    load(
+
+        const std::string& filename
+
+    );
+
+};
